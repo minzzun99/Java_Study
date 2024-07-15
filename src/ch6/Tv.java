44f73cd06da7243b0605743a35daa58d6f58158a
minzzun99 <skg06123@naver.com>
2024-07-15 16:45:53 +0900

    5장_배열_6.2.6_클래스의_또_다른_정의

---
+++ src/ch6/Tv.java
@@ -0,0 +1,19 @@
+package ch6;
+
+class Tv {
+    String color;
+    boolean power;
+    int channel;
+
+    void power() {
+        power = !power;
+    }
+
+    void channelUp() {
+        ++channel;
+    }
+
+    void channelDown() {
+        --channel;
+    }
+}
--- src/ch6/TvTest.java
+++ src/ch6/TvTest.java
@@ -1,22 +1,5 @@
 package ch6;
 
-class Tv {
-    String color;
-    boolean power;
-    int channel;
-
-    void power() {
-        power = !power;
-    }
-
-    void channelUp() {
-        ++channel;
-    }
-
-    void channelDown() {
-        --channel;
-    }
-}
 public class TvTest {
     public static void main(String[] args) {
         Tv t;
