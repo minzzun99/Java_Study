8bc2392bd7de4d4d5e60dc4ff3b196390904a885
minzzun99 <skg06123@naver.com>
2024-08-03 19:11:10 +0900

    7장 객체지향 프로그래밍2 8.4 내부 클래스의 제어자와 접근성

---
+++ src/ch7/InnerEx5.java
@@ -0,0 +1,25 @@
+package ch7;
+
+
+class Outer1 {
+    int value = 10;
+
+    class Inner {
+        int value = 20;
+
+        void method1() {
+            int value = 30;
+            System.out.println("            value : " + value);
+            System.out.println("       this.value : " + this.value);
+            System.out.println("Outer1.this.value : " + Outer1.this.value);
+        }
+    }
+}
+
+public class InnerEx5 {
+    public static void main(String[] args) {
+        Outer1 outer = new Outer1();
+        Outer1.Inner inner = outer.new Inner();
+        inner.method1();
+    }
+}
