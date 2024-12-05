245e4d7ad3ad88b482d6ed4ed66ce07684fc5780
minzzun99 <skg06123@naver.com>
2024-12-05 19:49:35 +0900

    12장 1.3 제네릭 클래스의 객체 생성과 사용
    - FruitBoxEx1

---
+++ src/ch12/FruitBoxEx1.java
@@ -0,0 +1,68 @@
+package ch12;
+
+import java.util.ArrayList;
+
+class Fruit {
+    public String toString() {
+        return "Fruit";
+    }
+}
+
+class Apple extends Fruit {
+    public String toString() {
+        return "Apple";
+    }
+}
+
+class Grape extends Fruit {
+    public String toString() {
+        return "Grape";
+    }
+}
+
+class Toy {
+    public String toString() {
+        return "Toy";
+    }
+}
+
+public class FruitBoxEx1 {
+    public static void main(String[] args) {
+        Box<Fruit> fruitBox = new Box<Fruit>();
+        Box<Apple> appleBox = new Box<>();
+        Box<Toy> toyBox = new Box<>();
+        Box<Grape> grapeBox = new Box<>();
+
+        fruitBox.add(new Fruit());
+        fruitBox.add(new Apple());
+
+        appleBox.add(new Apple());
+        appleBox.add(new Apple());
+
+        toyBox.add(new Toy());
+
+        System.out.println(fruitBox);
+        System.out.println(appleBox);
+        System.out.println(toyBox);
+    }
+}
+
+class Box<T> {
+    ArrayList<T> list = new ArrayList<>();
+
+    void add(T item) {
+        list.add(item);
+    }
+
+    T get(int i) {
+        return list.get(i);
+    }
+
+    int size() {
+        return list.size();
+    }
+
+    public String toString() {
+        return list.toString();
+    }
+}
